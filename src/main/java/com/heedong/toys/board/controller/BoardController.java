52e2ebd6434bf6041dbc9b861c818dda22fa9d9b
chaeheedong <qiip91@naver.com>
2021-10-11 22:33:30 +0900

    messages.properties 적용

--- src/main/java/com/heedong/toys/board/controller/BoardController.java
+++ src/main/java/com/heedong/toys/board/controller/BoardController.java
@@ -2,7 +2,9 @@ package com.heedong.toys.board.controller;
 
 import com.heedong.toys.board.dto.Board;
 import com.heedong.toys.board.service.BoardService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,7 +13,12 @@ import java.util.List;
 
 @Controller
 @RequestMapping("board")
+@Slf4j
 public class BoardController {
+
+    @Autowired
+    private MessageSource messageSource;
+    // messageSource.getMessage("error.404", null, Locale.getDefault())
 
     @Autowired
     private BoardService boardService;
